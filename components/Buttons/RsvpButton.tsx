760dbf87b4f5bb637086ce7cb48453de5235324c
Moana Sodergren <moana.sodergren@wbd.com>
2025-11-23 14:24:12 +0100

    fix implicit return

--- components/Buttons/RsvpButton.tsx
+++ components/Buttons/RsvpButton.tsx
@@ -3,22 +3,17 @@ import Button from "./Button";
 import { Typography, Box, Link } from "@mui/material";
 import React from "react";
 
-
 export default function RsvpButton(): JSX.Element {
   return (
     <Box
       sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
     >
       <Button
-          //  component={Link}
-          //  href={"https://docs.google.com/forms/d/e/1FAIpQLSfpAdkh6dO_8jLksPz14Vaia30HxyALseKrAU8kFPl0BkLIvQ/viewform?usp=header"}
-          //  target="_blank"
-          //  rel="noopener noreferrer"
-        onClick={() =>
+        onClick={() => {
           window.open(
             "https://docs.google.com/forms/d/e/1FAIpQLSfpAdkh6dO_8jLksPz14Vaia30HxyALseKrAU8kFPl0BkLIvQ/viewform?usp=header"
-          )
-        }
+          );
+        }}
       >
         <Typography variant="h4">RSVP</Typography>
       </Button>
